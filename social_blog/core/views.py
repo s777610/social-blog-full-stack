480c3e20f0e06d8124523d64678e7ec4c9550cb5
s777610 <s777610@gmail.com>
2018-12-02 06:37:44 -0800

    scss

--- social_blog/core/views.py
+++ social_blog/core/views.py
@@ -8,7 +8,7 @@ core = Blueprint('core', __name__)
 @core.route("/home")
 def index():
     page = request.args.get('page', 1, type=int)
-    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=3)
     return render_template('index.html', blog_posts=blog_posts)
 
 
